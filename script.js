1e069feddb5b19a0ea0f22427f5d617d1de32035
Lucre <josielindamaldonado@gmail.com>
2021-03-19 12:41:05 -0400

    Edited readme and began JS prompts.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-var Begin = alert("Let's create a cusotmized unique Generated Password!!!")
+var newLine = "\r\n"
+
+var begin = alert("Let's create a cusotmizable unique Password!!!");
+
+var password = prompt("How many character would you like your password?"
+ + "Choose between 8 and 128 characters.");
