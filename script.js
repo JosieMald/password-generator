9575d86d4b467167d7efadda971d10a38129c63a
Lucre <josielindamaldonado@gmail.com>
2021-03-19 21:52:32 -0400

    Added all prompts and alerts.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-var newLine = "\r\n"
+var newLine = "\n"
 
 var begin = alert("Let's create a cusotmizable unique Password!!!");
 
 var password = prompt("How many character would you like your password?"
- + "Choose between 8 and 128 characters.");
++ newLine +
+"Choose between 8 and 128 characters.");
+
+var alert = alert("Here we'll decide what kinda of character types you'd like for your password.");
+
+var lowerCase = prompt("Would you like to use lowercase letters?")
+
+var upperCase = prompt("Would you like to use Uppercase letters?");
+
+var numeric = prompt("Would you like to use numerical numbers?");
+
+var specialChar = prompt("Would you like to use special characters?" 
++ newLine + "Example: ! @ # $ % ^ & * ")
