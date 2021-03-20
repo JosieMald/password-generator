111399f2d3bd62e21064e0b5858d9100ea3cc6c5
Lucre <josielindamaldonado@gmail.com>
2021-03-19 22:36:50 -0400

    adding comments and organizing JS.

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+// <-- Variables -->
 var newLine = "\n"
 
-var begin = alert("Let's create a cusotmizable unique Password!!!");
 
+// <-- Start of code alert -->
+
+function password 
+function var begin = alert("Let's create a cusotmizable unique Password!!!"); {
+
+}
+
+// <-- Choose how many characters you'd like -->
 var password = prompt("How many character would you like your password?"
 + newLine +
 "Choose between 8 and 128 characters.");
 
+// <-- Alert to begin using characters  -->
 var alert = alert("Here we'll decide what kinda of character types you'd like for your password.");
 
+// <-- Lowercase letters prompt -->
 var lowerCase = prompt("Would you like to use lowercase letters?")
 
+// <-- Uppercase letters prompt -->
 var upperCase = prompt("Would you like to use Uppercase letters?");
 
+// <-- Numerical characters prompt -->
 var numeric = prompt("Would you like to use numerical numbers?");
 
+// <-- Special characters prompt -->
 var specialChar = prompt("Would you like to use special characters?" 
 + newLine + "Example: ! @ # $ % ^ & * ")
