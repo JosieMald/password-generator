57203ac45de4f3c26cafea31e30ad07634c7c50b
Lucre <josielindamaldonado@gmail.com>
2021-03-20 01:56:43 -0400

    added evenListener.

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 // <-- Variables -->
-var newLine = "\n"
+var newLine = "\n";
+var generateBtn = document.querySelector("button");
 
 
 // <-- Start of code alert -->
+generateBtn.addEventListener ("click", function() {
+    alert("hi");
+});
 
-function password 
-function var begin = alert("Let's create a cusotmizable unique Password!!!"); {
 
-}
+// generateBtn.addEventListener("click", function(answer) {
+//  alert("Let's create a cusotmizable unique Password!!!");
+
+// //  <-- Prompts begin -->
+//  if (answer === true) { 
+//      continue;
+//  } else {
+//      return;
+//  }
+// });
+
+// return;
 
 // <-- Choose how many characters you'd like -->
-var password = prompt("How many character would you like your password?"
-+ newLine +
-"Choose between 8 and 128 characters.");
 
-// <-- Alert to begin using characters  -->
-var alert = alert("Here we'll decide what kinda of character types you'd like for your password.");
+// var password = prompt("How many character would you like your password?"
+// + newLine +
+// "Choose between 8 and 128 characters.");
 
-// <-- Lowercase letters prompt -->
-var lowerCase = prompt("Would you like to use lowercase letters?")
+// // <-- Alert to begin using characters  -->
+// var alert = alert("Here we'll decide what kinda of character types you'd like for your password.");
 
-// <-- Uppercase letters prompt -->
-var upperCase = prompt("Would you like to use Uppercase letters?");
+// // <-- Lowercase letters prompt -->
+// var lowerCase = prompt("Would you like to use lowercase letters?")
 
-// <-- Numerical characters prompt -->
-var numeric = prompt("Would you like to use numerical numbers?");
+// // <-- Uppercase letters prompt -->
+// var upperCase = prompt("Would you like to use Uppercase letters?");
 
-// <-- Special characters prompt -->
-var specialChar = prompt("Would you like to use special characters?" 
-+ newLine + "Example: ! @ # $ % ^ & * ")
+// // <-- Numerical characters prompt -->
+// var numeric = prompt("Would you like to use numerical numbers?");
+
+// // <-- Special characters prompt -->
+// var specialChar = prompt("Would you like to use special characters?" 
+// + newLine + "Example: ! @ # $ % ^ & * ")
