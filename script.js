44d184aee51f830556b5cf1f385e4c1ef9562659
Lucre <josielindamaldonado@gmail.com>
2021-03-20 22:06:28 -0400

    Completed JS.

--- script.js
+++ script.js
@@ -1,45 +1,140 @@
-// <-- Variables -->
-var newLine = "\n";
-var generateBtn = document.querySelector("button");
 
+// <-- Variables -->
+var alpha = ["a",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i", 
+"j", 
+"k", 
+"l", 
+"m", 
+"n", 
+"o", 
+"p", 
+"q", 
+"r", 
+"s", 
+"t", 
+"u", 
+"v", 
+"w", 
+"x", 
+"y", 
+"z"];
+var generateBtn = document.querySelector("button");
+var character = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var answers = [];
+var lowCaseArray = [];
+var length;
+var passwordContainer = [];
 
 // <-- Start of code alert -->
-generateBtn.addEventListener ("click", function() {
-    alert("hi");
+
+generateBtn.addEventListener ("click", function () {
+    answers = [];
+    passwordContainer = [];
+    alert("Let's create a customizable password!");
+    characterLength();
 });
-
-
-// generateBtn.addEventListener("click", function(answer) {
-//  alert("Let's create a cusotmizable unique Password!!!");
-
-// //  <-- Prompts begin -->
-//  if (answer === true) { 
-//      continue;
-//  } else {
-//      return;
-//  }
-// });
-
-// return;
 
 // <-- Choose how many characters you'd like -->
 
-// var password = prompt("How many character would you like your password?"
-// + newLine +
-// "Choose between 8 and 128 characters.");
+function characterLength() {
+    length = prompt("Choose a length of characters between 8 and 129.");
+    console.log(length);
+    if (length < 8 || length > 129 ){
+        alert("Please choose a number between 8 and 129");
+        characterLength();
+    } else {
+        start();
+    }
+};
+// <-- Alert to begin using characters  -->
 
-// // <-- Alert to begin using characters  -->
-// var alert = alert("Here we'll decide what kinda of character types you'd like for your password.");
+function start() {
+    alert("Here we'll decide what kinda of character types you'd like for your password.");
+    lowerCase();
+};
 
-// // <-- Lowercase letters prompt -->
-// var lowerCase = prompt("Would you like to use lowercase letters?")
+// <-- Lowercase letters prompt -->
+// -- When true for loop will add lowercase letters to array --
 
-// // <-- Uppercase letters prompt -->
-// var upperCase = prompt("Would you like to use Uppercase letters?");
+function lowerCase() {
+    var lowCase = confirm("Would you like to use lowercase letters?");
+    console.log(lowCase);
+    if (lowCase === false) {
+        upperCase();
+    } else {
+        for (var i = 0; i < length; i++) {
+            answers.push(alpha[Math.floor(Math.random() * 26)]);
+        }
+        console.log(answers);
+        upperCase();
+    }
+};
 
-// // <-- Numerical characters prompt -->
-// var numeric = prompt("Would you like to use numerical numbers?");
+// <-- Uppercase letters prompt -->
+// -- When true for loop will add uppercase letters to array --
 
-// // <-- Special characters prompt -->
-// var specialChar = prompt("Would you like to use special characters?" 
-// + newLine + "Example: ! @ # $ % ^ & * ")
+function upperCase() {
+    var upCase = confirm("Would you like to use Uppercase letters?");
+    console.log(upCase);
+    if (upCase === false) {
+        numerical();
+    } else {
+        for (var i = 0; i < length; i++) {
+            answers.push(alpha[Math.floor(Math.random() * 26)].toUpperCase());
+        }
+        console.log(answers);
+        numerical();
+    }
+};
+
+// <-- Numerical characters prompt -->
+// -- When true for loop will add numbers to array --
+
+function numerical() {
+    var numbers = confirm("Would you like to use numerical numbers?");
+    console.log(numbers);
+    if (numbers === false) {
+        specialcharacters();
+    } else {
+        for (var i = 0; i < length; i++) {
+            answers.push(number[Math.floor(Math.random() * 10)]);
+        }
+        console.log(answers);
+        specialcharacters();
+    }
+};
+
+// <-- Special characters prompt -->
+// -- When true for loop will add special characters to array --
+
+function specialcharacters() {
+    var specialChar = confirm("Would you like to use special characters? \n Example: ! @ # $ % ^ & * ");
+    console.log(specialChar);
+    if (specialChar === false){
+        complete();
+    } else {
+        for (var i = 0; i < length; i++) {
+            answers.push(character[Math.floor(Math.random() * 8)]);
+        }
+        console.log(answers);
+        complete();
+    }
+};
+// <-- Displays password generator -->
+
+function complete() {
+    for (var i = 0; i < length; i++) {
+        passwordContainer.push(answers[Math.floor(Math.random() * answers.length)]);
+    }
+    var password = document.querySelector("textarea")
+    password.innerHTML = passwordContainer.join("");
+}
